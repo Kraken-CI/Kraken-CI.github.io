a6ef08786f493a099ccf46e3e46e2a08ed80fb07
Michal Nowikowski <godfryd@gmail.com>
2020-09-11 09:18:25 +0200

    Deploy website - based on ae84e7ac34b5a5cd61489abdd30f7c527fcca885

---
+++ dbe07fb5.a22329c8.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[23],{80:function(a){a.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"kraken","name":"kraken","count":1,"permalink":"/blog/tags/kraken"}')}}]);
