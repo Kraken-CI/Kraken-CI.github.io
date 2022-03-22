7d5af63e51c7c0bb0155df9f5079764c95fe88e3
Michal Nowikowski <godfryd@gmail.com>
2022-03-22 06:19:39 +0100

    Deploy website - based on 526b55db1ed7079545c0f30e02953fd0ed28f44f

---
+++ assets/js/dfbecd59.12d3dec9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkkraken_ci_website=self.webpackChunkkraken_ci_website||[]).push([[7059],{5053:function(e){e.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"/blog/tags/kraken","name":"kraken","count":13,"permalink":"/blog/tags/kraken"}')}}]);
