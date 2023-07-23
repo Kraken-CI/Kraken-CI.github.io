4bddad3967e06802f0ee2b7fae4d53c49a7aa51a
Michal Nowikowski <godfryd@gmail.com>
2023-07-23 07:18:24 +0200

    Deploy website - based on da761969da112ee345a6bbc53664512975bc8c35

--- assets/js/b93844a0.ef278194.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkkraken_ci_website=self.webpackChunkkraken_ci_website||[]).push([[2911],{5601:function(e){e.exports=JSON.parse('{"permalink":"/blog/tags/devops","page":1,"postsPerPage":10,"totalPages":2,"totalCount":11,"nextPage":"/blog/tags/devops/page/2","blogDescription":"Blog","blogTitle":"Blog"}')}}]);
